83ed0bcbb274521a619394bd1e7e933cbbfabd74
Samiran Majumder <samiran.majumder@kas-services.com>
2019-02-06 17:50:18 +0530

    add changes to trp docs

--- lib/ui/tripview/tripview.dart
+++ lib/ui/tripview/tripview.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 import 'package:kclaim/bottom_sheet_fix.dart';
 import 'package:kclaim/ui/starttripdialog/starttrip.dart';
 import 'package:kclaim/ui/tripdocumnet/tripdocs.dart';
-import 'package:path/path.dart';
-
 
 class TripScreen extends StatefulWidget {
   TripScreen({Key key, this.title}) : super(key: key);
@@ -15,31 +14,14 @@ class TripScreen extends StatefulWidget {
   _TripScreenState createState() => new _TripScreenState();
 }
 
-
 class _TripScreenState extends State<TripScreen> {
-
-   void addtrip(BuildContext context) {
-    // showModalBottomSheet<void>(
-    //   context: context,
-    //   builder: (BuildContext context) {
-    //     return TripDialog();
-    //   },
-    // );
-
-    showModalBottomSheetApp(  builder: (builder) { return  TripDialog(); }, context: context);
-   }
-
-  //   void _showMButtoModal() {
-  //   showModalBottomSheet<void>(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return enableUpload();
-  //     },
-  //   );
-  // }
-
- // showModalBottomSheetApp( context: context, builder: (builder) { return  addtrip(); });
-  
+  void addtrip(BuildContext context) {
+    showModalBottomSheetApp(
+        builder: (builder) {
+          return TripDialog();
+        },
+        context: context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,13 +38,12 @@ class _TripScreenState extends State<TripScreen> {
         builder: (context, snapshot) {
           if (!snapshot.hasData) return const Text("Loading....");
           return ListView.builder(
-            itemExtent: 150.0,
+            itemExtent: 130.0,
             itemCount: snapshot.data.documents.length,
             itemBuilder: (context, index) =>
                 _buildlistitem(context, snapshot.data.documents[index]),
           );
         },
-        
       )),
       floatingActionButton: FloatingActionButton(
         onPressed: (() {
@@ -73,40 +54,66 @@ class _TripScreenState extends State<TripScreen> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
+  }
 
-    
+  Widget cardRowDecor(BuildContext context, DocumentSnapshot document) {
+    return new Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 1.0,
+          horizontal: 4.0,
+        ),
+        child: new Stack(
+          children: <Widget>[
+            tripCard(context, document),
+            planetThumbnail,
+          ],
+        ));
   }
 
   _buildlistitem(BuildContext context, DocumentSnapshot document) {
     return Card(
-      elevation: 1.7,
+        elevation: 1.7,
         child: new Container(
-      child: cardRowDecor(context, document),
-    ));
+          child: cardRowDecor(context, document),
+        ));
   }
 
-  Widget planetCard(BuildContext context, DocumentSnapshot document) {
+  Widget tripCard(BuildContext context, DocumentSnapshot document) {
     return new Container(
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text(document['Tripname'], style: TextStyle(color: Colors.white.withOpacity(1.0)),),
-            subtitle: Text(document['TripStartDate'].toString(),style: TextStyle(color: Colors.white.withOpacity(1.0))),
-          ),
+              title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                document['Tripname'],
+                style: TextStyle(color: Colors.white.withOpacity(1.0)),
+              ),
+              Text(new DateFormat.yMMMd().format(document['TripStartDate']),
+                  style: TextStyle(color: Colors.white.withOpacity(1.0)))
+            ],
+          )),
           ButtonTheme.bar(
             // make buttons use the appropriate styles for cards
             child: ButtonBar(
               children: <Widget>[
-                Icon(Icons.update,
-                color: Colors.deepOrangeAccent,
+                Icon(
+                  Icons.update,
+                  color: Colors.deepOrangeAccent,
                 ),
                 FlatButton(
-                  child: const Text('UPDATE', style: TextStyle(color: Colors.black),),
+                  child: const Text(
+                    'UPDATE',
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onPressed: () {
-                  Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => MyTripDocWidget(tripId:document.documentID)),
-                      );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              MyTripDocWidget(tripId: document.documentID)),
+                    );
                   },
                 ),
               ],
@@ -114,17 +121,17 @@ class _TripScreenState extends State<TripScreen> {
           ),
         ],
       ),
-      height: 150.0,
+      height: 120.0,
       margin: new EdgeInsets.only(left: 46.0),
       decoration: new BoxDecoration(
-        color:  Colors.grey, //(0xFF333366),
+        color: Colors.grey, //(0xFF333366),
         shape: BoxShape.rectangle,
         borderRadius: new BorderRadius.circular(8.0),
         boxShadow: <BoxShadow>[
           new BoxShadow(
             color: Colors.black12,
             blurRadius: 10.0,
-            offset: new Offset(0.0, 10.0),
+            offset: new Offset(0.0, 11.0),
           ),
         ],
       ),
@@ -135,93 +142,8 @@ class _TripScreenState extends State<TripScreen> {
     margin: new EdgeInsets.symmetric(vertical: 16.0),
     alignment: FractionalOffset.centerLeft,
     child: Icon(
-      Icons.flight_takeoff,color: Colors.deepOrangeAccent,
+      Icons.flight_takeoff,
+      color: Colors.deepOrangeAccent,
     ),
   );
-
-
-  Widget cardRowDecor(BuildContext context, DocumentSnapshot document) {
-    return new Container(
-        // height: 180.0,
-        margin: const EdgeInsets.symmetric(
-          vertical: 1.0,
-          horizontal: 4.0,
-        ),
-        child: new Stack(
-          children: <Widget>[
-            planetCard(context, document),
-            planetThumbnail,
-          ],
-        ));
-  }
 }
-
-// class CardReowDecor extends StatelessWidget {
-//   BuildContext context;
-//   DocumentSnapshot document;
-//   CardRowDecor(BuildContext context, DocumentSnapshot document) {
-//     context = context;
-//     document = document;
-//   }
-
-//   final planetCard = new Container(
-//     child: Column(
-//       children: <Widget>[
-//         ListTile(
-//           title: Text('Tripname'),
-//           subtitle: Text('TripStartDate'),
-//         ),
-//         ButtonTheme.bar(
-//           // make buttons use the appropriate styles for cards
-//           child: ButtonBar(
-//             children: <Widget>[
-//               Icon(Icons.update),
-//               FlatButton(
-//                 child: const Text('UPDATE'),
-//                 onPressed: () {/* ... */},
-//               ),
-//             ],
-//           ),
-//         ),
-//       ],
-//     ),
-//     height: 150.0,
-//     margin: new EdgeInsets.only(left: 46.0),
-//     decoration: new BoxDecoration(
-//       color: new Color(0xFF333366),
-//       shape: BoxShape.rectangle,
-//       borderRadius: new BorderRadius.circular(8.0),
-//       boxShadow: <BoxShadow>[
-//         new BoxShadow(
-//           color: Colors.black12,
-//           blurRadius: 10.0,
-//           offset: new Offset(0.0, 10.0),
-//         ),
-//       ],
-//     ),
-//   );
-
-//   final planetThumbnail = new Container(
-//     margin: new EdgeInsets.symmetric(vertical: 16.0),
-//     alignment: FractionalOffset.centerLeft,
-//     child: Icon(
-//       Icons.account_balance,
-//     ),
-//   );
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Container(
-//         // height: 180.0,
-//         margin: const EdgeInsets.symmetric(
-//           vertical: 1.0,
-//           horizontal: 4.0,
-//         ),
-//         child: new Stack(
-//           children: <Widget>[
-//             planetCard,
-//             planetThumbnail,
-//           ],
-//         ));
-//   }
-// }
